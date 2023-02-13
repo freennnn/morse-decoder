2dcbeb4773a5094f2834dcef0ef686821df31a4d
freen <freennnn@gmail.com>
2023-02-13 13:23:23 -0800

    feat: morse alg implementation

--- src/index.js
+++ src/index.js
@@ -38,8 +38,47 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    function binaryToMorseToEncoded(str) {
+        if (str.length < 10) {
+            str = str.padStart(10, "0");
+        }
+        // 10 stands for dot(.), 11 stands for dash(-).
+        // Space in string is **********
+        let morse = "";
+        for (let i=0; i < str.length-1; i+= 2) {
+            let twobits = str.slice(i,i+2);
+            switch (twobits) {
+                case "10":
+                    morse += ".";
+                    break;
+                case "11":
+                    morse += "-";
+                    break;
+                case "**":
+                    morse += "**"
+                    break;
+                default:
+                    break;
+            }
+        }
+        
+        if (morse === "**********") {
+            return " ";
+        } else if (morse in MORSE_TABLE) {
+            return MORSE_TABLE[morse];
+        } else {
+            return "";
+        }
+    }
+    let decodedExpr = "";
+    for (i=0; i< expr.length; i+= 10) {
+        let encodedSymbol = expr.slice(i, i+10);
+        decodedExpr += binaryToMorseToEncoded(encodedSymbol);
+    }
+    return decodedExpr;
+    
 }
+
 
 module.exports = {
     decode
